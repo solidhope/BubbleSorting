c9b4341058f1500b2d17b4172c0bbc1f4f993e6e
Justin P Hogue <hogue.justin@gmail.com>
2013-04-01 18:29:16 -0300

    Comments

--- alpha/alpha/main.cpp
+++ alpha/alpha/main.cpp
@@ -1,3 +1,8 @@
+/*
+1. Compare each pair of adjacent elements from the beginning of an array and, if they are in reversed order, swap them.
+2. If at least one swap has been done, repeat step 1.
+*/
+
 #pragma once
 #include <iostream>
 #include "BubbleSort.h"
